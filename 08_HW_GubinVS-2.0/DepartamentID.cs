eaddd4ac224f7a6da241da431de414eff692a996
Владимир Губин <gubinvs@gmail.com>
2021-03-15 12:44:50 +0500

    о

---
+++ 08_HW_GubinVS-2.0/DepartamentID.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08_HW_GubinVS_2._0
+{
+    /// <summary>
+    /// Класс содержит коллекцию идентификаторов сотрудников с привязкой к идентификатору департамента.
+    /// Идентификатор департамента содержит список идентификаторов сотрудников, которые приписаны к данному департаменту
+    /// </summary>
+    class CompanyID
+    {
+        /// <summary>
+        /// Идентификаторы Workers
+        /// </summary>
+        public Guid WorkerID { get; set; }
+
+        /// <summary>
+        /// Идентификаторы Departaments
+        /// </summary>
+        public Guid DepartamentID { get; set; }
+
+        /// <summary>
+        /// Коллекция идентификаторов сотрудников с привязкой к идентификатору департамента
+        /// </summary>
+        public List<CompanyID> CompanyIDs { get; set; }
+
+
+        public CompanyID() 
+        {
+            this.CompanyIDs = new List<CompanyID>();
+        }
+
+
+
+    }
+}
